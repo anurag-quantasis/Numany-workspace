e848a65cff78ee537f2641d8a4fe001aef89e8cb
Clive1225 <clive.alphonso@quantasis.com>
2025-07-11 20:32:30 +0530

    remote-inventory, remote inventory module ts, remote inventory store ts

--- projects/tenant-numany/src/app/features/remote-inventory-location/remote-inventory-location.component.ts
+++ projects/tenant-numany/src/app/features/remote-inventory-location/remote-inventory-location.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { FormBuilder  } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { SelectModule } from 'primeng/select';
-import { FormGroup } from '@angular/forms';
-import { Validators } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
 import { DividerModule } from 'primeng/divider';
 import { CardModule } from 'primeng/card';
+import { SharedDataTableComponent, ColumnDef } from 'shared-ui';
+import { InventoryItem } from './remote-inventory-store/remote-inventory.model';
+import { RemoteInventoryLocationStore } from './remote-inventory-store/remote-inventory.store';
+import { ButtonModule } from 'primeng/button';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-remote-inventory-location',
@@ -14,26 +16,82 @@ import { CardModule } from 'primeng/card';
   templateUrl: './remote-inventory-location.component.html',
   imports: [
     CommonModule,
-    ReactiveFormsModule,
     SelectModule,
     DividerModule,
     CardModule,
+    SharedDataTableComponent,
+    ButtonModule,
+    ReactiveFormsModule
   ],
 })
 export class RemoteInventoryLocationComponent {
   remoteInventoryLocation: FormGroup;
 
   locationIDs = [
-    { name: 'Option1' },
-    { name: 'Option2' },
-    { name: 'Option3' },
-    { name: 'Option4' },
-    { name: 'Option5' }
+    { name: '2E' },
+    { name: '3NORTH' },
+    { name: 'adm machin' },
+    { name: 'BSONMERP' },
+    { name: 'CICU' },
   ];
 
-  constructor(private fb: FormBuilder) {
+  readonly columns: ColumnDef<InventoryItem>[] = [
+    { field: 'medName', header: 'MEDs in Remote' },
+    { field: 'unit', header: 'UNIT' },
+    { field: 'bin', header: 'BIN' },
+    { field: 'qtyLoaded', header: 'Qty Loaded' },
+    { field: 'lastLoaded', header: 'Last Loaded' },
+    { field: 'expires', header: 'Expires' },
+  ];
+
+  constructor(private fb: FormBuilder, public store: RemoteInventoryLocationStore) {
     this.remoteInventoryLocation = this.fb.group({
-      locationID: [null, Validators.required],
+      locationID: [null, [Validators.required, Validators.maxLength(10)]],
+      binID: ['', Validators.maxLength(10)],
+      qtyLoaded: ['', Validators.pattern(/^[0-9]*$/)],
+      lastLoaded: ['', this.dateValidator()],
+      expires: ['', this.dateValidator()],
     });
+  }
+
+  get selectedLocation() {
+    return this.remoteInventoryLocation.value.locationID;
+  }
+
+  dateValidator(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      if (!control.value) return null;
+      const isValid = !isNaN(Date.parse(control.value));
+      return isValid ? null : { invalidDate: true };
+    };
+  }
+
+  handleSelectionChange(selected: InventoryItem) {
+    this.store.selectItem(selected);
+  }
+
+  onAddItem() {
+    if (this.remoteInventoryLocation.invalid) return;
+    const form = this.remoteInventoryLocation.value;
+
+    this.store.addItem({
+      id: crypto.randomUUID(),
+      medName: 'New Medication',
+      unit: '1 TAB',
+      bin: form.binID || '',
+      qtyLoaded: form.qtyLoaded || '',
+      lastLoaded: form.lastLoaded || '',
+      expires: form.expires || '',
+    });
+  }
+
+  onDeleteItem() {
+    this.store.deleteSelected();
+  }
+
+  onChangeLocationID() {
+    const newID = prompt('Enter new Location ID (max 10 chars):');
+    if (!newID || newID.length > 10) return alert('Invalid ID.');
+    this.remoteInventoryLocation.patchValue({ locationID: newID });
   }
 }
---
+++ projects/tenant-numany/src/app/features/remote-inventory-location/remote-inventory-store/remote-inventory.model.ts
@@ -0,0 +1,9 @@
+export interface InventoryItem {
+  id: string;
+  medName: string;
+  unit: string;
+  bin: string;
+  qtyLoaded: string;
+  lastLoaded: string;
+  expires: string;
+}
