e93d1a4ea222220df85e3e0a50d841b4c0d54928
Rishi Singh <rishi.singh@quantasis.com>
2025-07-16 18:30:31 +0530

    added validations according to the vb6 code for the departments names

--- projects/tenant-numany/src/app/features/departments/departments.component.ts
+++ projects/tenant-numany/src/app/features/departments/departments.component.ts
@@ -4,11 +4,14 @@ import { InputTextModule } from 'primeng/inputtext';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { ShortcutDirective, SharedPanelContainerComponent, CustomInputComponent } from 'shared-ui';
-import { Fieldset } from "primeng/fieldset";
+import { FieldsetModule } from 'primeng/fieldset';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'tenant-departments',
+  standalone: true, // Assuming this is a standalone component
   imports: [
+    CommonModule, // Add CommonModule for *ngIf
     SelectModule,
     InputTextModule,
     ReactiveFormsModule,
@@ -16,8 +19,8 @@ import { Fieldset } from "primeng/fieldset";
     ShortcutDirective,
     SharedPanelContainerComponent,
     CustomInputComponent,
-    Fieldset
-],
+    FieldsetModule,
+  ],
   templateUrl: './departments.component.html',
   styleUrl: './departments.component.css',
 })
@@ -36,18 +39,23 @@ export class DepartmentsComponent implements OnInit {
   ngOnInit(): void {
     this.departmentForm = this.fb.group({
       department: ['', Validators.required],
-      id: ['', Validators.required],
-      name: ['', Validators.required],
-      address: ['', Validators.required],
-      city: ['', Validators.required],
-      state: ['', Validators.required],
-      zip: [null, Validators.required],
-      phone: ['', Validators.required],
-      note: ['', Validators.required],
-      cost_basis: ['', Validators.required],
-      markup: ['', Validators.required],
-      tax_flag: ['', Validators.required],
+      id: ['', [Validators.required, Validators.maxLength(6)]],
+      name: ['', [Validators.required, Validators.maxLength(30)]],
+      address: ['', Validators.maxLength(80)],
+      city: ['', Validators.maxLength(50)],
+      state: [''],
+      zip: [null, Validators.maxLength(10)],
+      phone: ['', Validators.maxLength(20)],
+      note: ['', Validators.maxLength(50)],
+      cost_basis: [''],
+      markup: [''],
+      tax_flag: [''],
     });
+
+    // Note: The 'id' and 'name' controls should probably be disabled
+    // if you always want them to be populated from the dropdown.
+    // this.departmentForm.get('id')?.disable();
+    // this.departmentForm.get('name')?.disable();
 
     this.departmentForm.get('department')!.valueChanges.subscribe((selectedId) => {
       const selectedDept = this.departmentOptions.find((dept) => dept.id === selectedId);
@@ -63,7 +71,13 @@ export class DepartmentsComponent implements OnInit {
   }
 
   printForm(): void {
-    console.log(this.departmentForm.value);
+    if (this.departmentForm.valid) {
+      console.log(this.departmentForm.value);
+      alert('Form Submitted! Check console for data.');
+    } else {
+      console.error('Form is invalid');
+      this.departmentForm.markAllAsTouched();
+    }
   }
 
   resetForm(): void {
